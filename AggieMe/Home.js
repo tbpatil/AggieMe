aa6c591ea63ea8c2152735f4881c9b1c863c071f
Toniya <91711219+tbpatil@users.noreply.github.com>
2024-02-21 18:57:29 -0800

    Create Home.js

---
+++ AggieMe/Home.js
@@ -0,0 +1,22 @@
+// file to navigate to different pages directly (if you're commiting to main)
+import React from 'react';
+import { View, Button } from 'react-native';
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {/* Other components */}
+      <Button
+        title="Go to Registration"
+        onPress={() => navigation.navigate('Registration')}
+      />
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('ProfileScreen')}
+      />
+      {/* Add navigation to other screens as needed */}
+    </View>
+  );
+}
+
+export default Home;
